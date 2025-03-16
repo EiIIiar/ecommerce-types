b1ffe3aa5dc914d3469797600dce72e3eb354e34
rodrigo godoi <rodrigo.godoi@totvspartners.com.br>
2025-03-16 18:09:43 -0300

    fix: lib

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,16 @@
-export * as index from "./types/user";
+export declare const role: Record<string, string>;
+export type TKeyRole = keyof typeof role;
+export type TValueRoles = (typeof role)[keyof typeof role];
+export interface IUser {
+    id: string;
+    userName: string;
+    password: string;
+    role: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+export interface IUserCreate extends Omit<IUser, "id" | "createdAt" | "updatedAt"> {
+}
+export interface IUserUpdate extends Partial<Omit<IUser, "id" | "createdAt" | "updatedAt" | "userName">> {
+}
 //# sourceMappingURL=index.d.ts.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-export * as index from "./types/user";
+// role
+export const role: Record<string, string> = {
+  admin: "admin",
+  user: "user",
+} as const;
+
+export type TKeyRole = keyof typeof role;
+
+export type TValueRoles = (typeof role)[keyof typeof role];
+
+// user
+export interface IUser {
+  id: string;
+  userName: string;
+  password: string;
+  role: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+// Interface for create (id, createdAt, and updatedAt are omitted)
+export interface IUserCreate extends Omit<IUser, "id" | "createdAt" | "updatedAt"> {}
+
+// Interface for update (userName is omitted, other fields are optional)
+export interface IUserUpdate
+  extends Partial<Omit<IUser, "id" | "createdAt" | "updatedAt" | "userName">> {}
