e8cff186663d557495113b7eafbecb3a9dc1d5f9
rodrigo godoi <rodrigo.godoi@totvspartners.com.br>
2025-03-16 17:24:10 -0300

    fix: lib

--- src/index.ts
+++
@@ -1,2 +0,0 @@
-// Exportando os tipos para torná-los acessíveis fora do módulo
-export * from "./types/user";
--- src/types/user.ts
+++
@@ -1,8 +0,0 @@
-export interface IUser {
-  id: string;
-  userName: string;
-  password: string;
-  role: string;
-  createdAt: Date;
-  updatedAt: Date;
-}
